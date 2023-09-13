0a3d0468f7b6dbac674131dfb70ddb241069dafa
nikolas-con <nikos.a.constantinou@gmail.com>
2023-09-13 20:40:54 +0300

    magic password

--- src/canister_upgreade_backend/src/lib.rs
+++ src/canister_upgreade_backend/src/lib.rs
@@ -2,6 +2,7 @@ use candid::Principal;
 use ic_cdk_macros::update;
 use ic_cdk::api::management_canister::main::*;
 
+
 #[update]
 fn upgrade() {
     let canister_id = ic_cdk::id();
@@ -11,28 +12,10 @@ fn upgrade() {
         mode: CanisterInstallMode::Upgrade,
         wasm_module: wasm.to_vec(),
         canister_id: canister_id,
-        arg: vec![]
+        arg: ic_cdk::api::id().as_slice().to_vec(),
     };
     ic_cdk::notify::<_>(Principal::management_canister(), "install_code", (install_arg,)).unwrap();
-    // ic_cdk::call::<_, ((),)>(Principal::management_canister(), "install_code", (install_arg,)).await.unwrap();
 }
-
-
-#[update]
-async fn set_controller() {
-    let canister_id = ic_cdk::id();
-    let caller = ic_cdk::caller();
-    let update_settings_argument = UpdateSettingsArgument{
-        canister_id: canister_id, 
-        settings: CanisterSettings { 
-            controllers: Some(vec![canister_id, caller]),
-            compute_allocation: None,
-            memory_allocation: None,
-            freezing_threshold: None 
-    }};
-    ic_cdk::call::<_, ((),)>(Principal::management_canister(), "update_settings", (update_settings_argument,)).await.unwrap();
-}
-
 
 #[update]
 fn version_1() {

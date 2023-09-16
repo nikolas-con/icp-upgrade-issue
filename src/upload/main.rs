096c11d73806b213a7216e0bd54ac65524774798
Andreas Tzionis <andy0n7@gmail.com>
2023-09-16 16:30:49 +0300

    more work on upload script

--- src/upload/main.rs
+++ src/upload/main.rs
@@ -15,30 +15,27 @@ async fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    // get agent url
+    // get network name
     let networks = HashMap::from([
-        ("ic".to_owned(), "https://ic0.app".to_owned()),
-        ("local".to_owned(), "http://127.0.0.1:8000".to_owned())
+        ("ic".to_owned(), ("https://ic0.app".to_owned(), ".canister_ids.json".to_owned())),
+        ("local".to_owned(), ("http://127.0.0.1:8000".to_owned(), ".dfx/local/canister_ids.json".to_owned()))
     ]);
     let network_name_default = "local".to_owned();
     let network_name_opt = args.iter().position(|r| r == "--network").map(|i| args.get(i + 1).unwrap());
     let network_name = network_name_opt.unwrap_or(&network_name_default);
-    let agent_url = networks.get(network_name).unwrap();
-    dbg!(agent_url);
-
-    return;
-
-    // get canister id
-    let file = File::open(".dfx/local/canister_ids.json").unwrap();
-    let json: serde_json::Value = serde_json::from_reader(file).unwrap();
-    let networks = json.get("backend").unwrap();
-    let local = networks.get("local").unwrap().as_str().unwrap();
-    let canister_id = local.to_string();
 
     // get agent
-    let canister = Principal::from_text(&canister_id).unwrap();
-    let agent = Agent::builder().with_url(agent_url).build().unwrap();
+    let (agent_url, _) = networks.get(network_name).unwrap();
+    let agent = Agent::builder().with_url(agent_url.as_str()).build().unwrap();
     agent.fetch_root_key().await.unwrap();
+
+    // get canister
+    let (_, canister_path) = networks.get(network_name).unwrap();
+    let file = File::open(canister_path).unwrap();
+    let json: serde_json::Value = serde_json::from_reader(file).unwrap();
+    let networks = json.get("backend").unwrap();
+    let canister_id = networks.get(network_name).unwrap().as_str().unwrap();
+    let canister = Principal::from_text(&canister_id.to_string()).unwrap();
     
     // upgrade canister
     agent.update(&canister, "upgrade").with_arg(Encode!(&WASM.to_vec()).unwrap()).call_and_wait().await.unwrap();
